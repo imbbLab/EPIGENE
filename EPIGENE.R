5e6bcbc8162936560e8b0afe0bf719ad0221966c
Anshupa <anshupa.vssut@gmail.com>
2019-10-28 11:31:42 +0000

    Update EPIGENE.R

--- EPIGENE.R
+++ EPIGENE.R
@@ -13,6 +13,8 @@ fileLoc = strsplit(grep('--filePath', args, value = TRUE), split = '=')[[1]][[2]
 #load all the relevant packages
 #####################################################################################
 
+source(paste0(fileLoc,"BIN/","functions.R"))
+
 pkgLoad("ggplot2")
 pkgLoad("GenomicFeatures")
 pkgLoad("BSgenome.Hsapiens.UCSC.hg19")
@@ -21,6 +23,7 @@ pkgLoad("GenomicRanges")
 pkgLoad("EPI.genefinder")
 pkgLoad("normr")
 pkgLoad("bamsignals")
+pkgLoad("RColorBrewer")
 
 #####################################################################################
 #genome binning
@@ -46,7 +49,7 @@ gContigs = binInfo[[2]]
 #obtain the read counts for genomic bins
 #####################################################################################
 
-featureInfo = read.table(seqDat,header = T,sep = "\t",col.names = c("features","location","sequencing"))
+featureInfo = read.table(seqDat,sep = "\t",col.names = c("features","location","sequencing"))
 
 features = as.character(featureInfo$features)
 trFPath = as.character(featureInfo$location)
@@ -72,8 +75,7 @@ runNormR = list(
   H3K36me3 = c("H3K36me3", "HM.input"),
   H3K4me1 = c("H3K4me1", "HM.input"),
   H3K4me3 = c("H3K4me3", "HM.input"),
-  H3K9me3 = c("H3K9me3", "HM.input"),
-  Pol2 = c("Pol2", "Pol2.input")
+  H3K9me3 = c("H3K9me3", "HM.input")
 )
 
 norm = mclapply(runNormR, function(run) {
@@ -83,6 +85,45 @@ norm = mclapply(runNormR, function(run) {
 clzz = sapply(norm, function(x) { cl = normr::getClasses(x, fdr = 0.2); cl[!is.finite(cl)] = 0; cl})
 
 #####################################################################################
+#create updateEmission and updateTransition
+#####################################################################################
+
+emissionProb = readRDS(paste0(fileLoc,"/DATA/","emissionProb.rds"))
+transitionMat = readRDS(paste0(fileLoc,"/DATA/","transitionMat.rds"))
+updateEmission = c(rep(FALSE, 14), rep(TRUE, 3))
+updateTransition = matrix(FALSE, ncol = 17, nrow = 17)
+updateTransition[1, 15:17] = TRUE
+updateTransition[15:17, 7] = TRUE
+updateTransition[14, 15:17] = TRUE
+updateTransition[15:17, 8] = TRUE
+
+updateTransition[15:17, 15:17] = TRUE
+
+updateTransition[14,7] = TRUE
+updateTransition[1,7] = TRUE
+updateTransition[14,8] = TRUE
+updateTransition[1,8] = TRUE
+
+#####################################################################################
+#obtain length of contigs
+#####################################################################################
+
+gMap <- findOverlaps(bins,gContigs,type = "within")
+
+qHits <- queryHits(gMap)
+sHits <- subjectHits(gMap)
+mapContigs <- data.frame(qHits,sHits)
+colnames(mapContigs) <- c("QueryHits","SubjectHits")
+
+seqLenList <- c()
+
+for(i in 1:length(gContigs)){
+  seqLenList <- c(seqLenList,nrow(mapContigs[which(mapContigs$SubjectHits==i),]))
+}
+
+names(seqLenList) <- c(1:length(gContigs))
+
+#####################################################################################
 #predicting transcription units
 #####################################################################################
 
@@ -90,6 +131,11 @@ train = EPI.genefinder(clzz[, 1:6], emissionProb, NULL, t(transitionMat), seqLen
                        t(updateTransition), maxiter = 500, nthreads = 24)
 labels = c("tss", "firstExon", "firstIntron", "iExon", "iIntron", "lastExon", "tts", "rtss",
            "rfirstExon", "rfirstIntron", "riExon", "riIntron", "rlastExon", "rtts", "bg1", "bg2", "bg3")
+
+tssFwd <- which(train$viterbi$vpath == 1)
+ttsFwd <- which(train$viterbi$vpath == 7)
+tssRev <- which(train$viterbi$vpath == 14)
+ttsRev <- which(train$viterbi$vpath == 8)
 
 mapData = data.frame("bin"= queryHits(gMap),"contig"=subjectHits(gMap))
 transFwd <- mapTssTts(tssFwd,ttsFwd,mapData)
@@ -113,6 +159,7 @@ endDat <- endList[tmpData$TTS]
 chrDat <- chrList[tmpData$TSS]
 
 gPredictions <- GRanges(chrDat,IRanges(start = startDat,end = endDat),strand = tmpData$Strand)
+export.bed(gPredictions,con = paste0(fileLoc,"TU_predictions.bed"))
 
 #####################################################################################
 #visualise estimated parameters
@@ -127,8 +174,9 @@ emission[,"Histone Marks"] = labelsX
 plotEM = reshape2::melt(emission)
 colnames(plotEM) = c("HistoneMarks","States","Probability")
 myPalette <- colorRampPalette(rev(brewer.pal(10, "Spectral")))
-ggplot(plotEM,aes(HistoneMarks,States))+geom_tile(aes(fill = Probability),colour = "black")+scale_fill_gradient(low = "white",high = "darkgreen")+
+estParam = ggplot(plotEM,aes(HistoneMarks,States))+geom_tile(aes(fill = Probability),colour = "black")+scale_fill_gradient(low = "white",high = "darkgreen")+
   theme_bw()+labs(y = "States")+
   theme(axis.text.x = element_text(size = 10,angle = 45, hjust = 1),axis.text.y = element_text(size = 10),
         axis.title.x = element_blank(),axis.title.y = element_text(size = 10),
         legend.text = element_text(size = 10),legend.title = element_text(size = 10))
+ggsave(estParam,filename = paste0(fileLoc,"estimated_params.pdf"))
